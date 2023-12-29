84e328485fa06326c93c15bdcc569f71567cd032
Yeifreng <yeifrenjose@gmail.com>
2023-12-29 08:15:19 -0500

    creando los componentes de busqueda y lista de gif de la home page

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    HomeComponent
   ]
 })
 export class GifsModule { }
