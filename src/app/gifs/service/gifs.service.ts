e45b64c21f770f6819e0ce6608241e8434323f4b
Yeifreng <yeifrenjose@gmail.com>
2024-01-02 13:27:44 -0500

    persistiendo datos en localStorage

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -13,7 +13,9 @@ export class GifsService {
   private apiKey: string = 'NkEP8JmPiLRJBOpbGVb7kKDkaHZmz9Ma';
   private url: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private httpClient:HttpClient){ }
+  constructor(private httpClient:HttpClient){
+    this.lowLocalStorage();
+   }
 
   get tagsHistory(){
     return this._tagsHistory;
@@ -30,8 +32,18 @@ export class GifsService {
     this.saveLocalStorage();
   }
 
-  saveLocalStorage():void{
+  private saveLocalStorage():void{
     localStorage.setItem('history', JSON.stringify (this._tagsHistory));
+  }
+
+  private lowLocalStorage():void{
+    if(!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this.tagsHistory[0]);
+
   }
 
   searchTag(tag:string){
